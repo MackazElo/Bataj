fb79cad354427ea1c2c3bdf8e2a6acc6437d742e
kuba <kuba@kuba-glowny.lan>
2023-05-17 14:33:38 +0200

    first commit hehe

---
+++ database.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	host : '192.168.2.222',
+	database : 'batmark2',
+	user : 'root',
+	password : ''
+});
+
+// connection.connect(function(error){
+// 	if(error)
+// 	{
+// 		throw error;
+// 	}
+// 	else
+// 	{
+// 		console.log('MySQL Database is connected Successfully');
+// 	}
+// });
+
+module.exports = connection;
